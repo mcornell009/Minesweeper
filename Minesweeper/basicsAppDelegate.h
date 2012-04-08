daac0256d91d78b7c7f0c9eb3619894b630dc086
Michael Cornell <mike.cornell.009@gmail.com>
2012-04-07 23:46:10 -0400

    Initial Commit

---
+++ Minesweeper/basicsAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  basicsAppDelegate.h
+//  Minesweeper
+//
+//  Created by Michael Cornell on 4/7/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface basicsAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Minesweeper/basicsViewController.h
@@ -0,0 +1,13 @@
+//
+//  basicsViewController.h
+//  Minesweeper
+//
+//  Created by Michael Cornell on 4/7/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface basicsViewController : UIViewController
+
+@end
