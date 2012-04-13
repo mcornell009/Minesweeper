c4d91d15546f346d435662d253dcb87ba1b8e547
Michael Cornell <mike.cornell.009@gmail.com>
2012-04-12 20:57:09 -0400

    first commit

--- Minesweeper/basicsViewController.h
+++ Minesweeper/basicsViewController.h
@@ -8,6 +8,24 @@
 
 #import <UIKit/UIKit.h>
 
+NSInteger bombArray[10];
+UIButton *buttonArray[100];
+
 @interface basicsViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UIButton *resetGame;
+@property (weak, nonatomic) IBOutlet UILabel *scoreLabel;
+@property (weak, nonatomic) IBOutlet UILabel *timeLabel;
+@property (weak, nonatomic) IBOutlet UILabel *endLabel;
+@property (weak, nonatomic) IBOutlet UIButton *flagButton;
+@property int time;
+@property int score;
+@property BOOL flagging;
+-(void)newGame;
+-(void)buttonClicked:(UIButton*)button;
+-(void)mineHit;
+-(void)cleanHit:(UIButton*)button;
+-(void)generateMines:(int)mine;
+-(BOOL)isMined:(int)index;
+
 
 @end
--- Minesweeper/basicsViewController.m
+++ Minesweeper/basicsViewController.m
@@ -9,52 +9,303 @@
 #import "basicsViewController.h"
 
 @implementation basicsViewController
+@synthesize resetGame;
+@synthesize scoreLabel;
+@synthesize timeLabel;
+@synthesize endLabel;
+@synthesize flagButton;
+@synthesize time;
+@synthesize score;
+@synthesize flagging;
 
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Release any cached data, images, etc that aren't in use.
+-(void)newGame{
+    endLabel.text=nil;
+    scoreLabel.text=@"Score";
+    score=0;
+    flagging=NO;
+    [flagButton setTitle:@"Flag" forState:UIControlStateNormal];   
+    int index=0;
+    int yAxis=70;
+    for(int y=0;y<100;y=y+10){
+        int xAxis=20;
+        for( int x = 1; x < 11; x++) {
+            buttonArray[index] = [[UIButton alloc]init];
+            buttonArray[index] = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+            [buttonArray[index] setTag:index];
+            [buttonArray[index] addTarget:self action:@selector(buttonClicked:) forControlEvents:UIControlEventTouchUpInside];
+            buttonArray[index].frame = CGRectMake(xAxis, yAxis, 26.0, 26.0);
+            [self.view addSubview:buttonArray[index]];
+            
+            xAxis=xAxis+28;
+            index=x+y;
+        }
+        yAxis=yAxis+28;
+    }
+    //generate bombs...
+    //RECURSIVELY BOOYAH BENJAMIN
+    for (int c=0;c<10;c++){
+    [self generateMines:c];
+    }
 }
-
-#pragma mark - View lifecycle
-
+- (IBAction)flagPress:(id)sender{
+    if (flagging==YES){
+     flagging=NO;
+        [flagButton setTitle:@"Flag" forState:UIControlStateNormal];   
+    }
+    else {
+        flagging=YES;
+        [flagButton setTitle:@"Flagging" forState:UIControlStateNormal];   
+    }
+}
+-(void)generateMines:(int)mine{
+    bombArray[mine]= (arc4random()%99);
+    for (int bombs=0;bombs<mine;bombs++){
+        if (bombArray[bombs]==bombArray[mine]){
+            bombArray[mine]= (arc4random()%99);
+            bombs=0;
+        }
+    }
+    NSLog(@"BOMB AT %d",bombArray[mine]);
+}
+- (IBAction)resetPress:(id)sender {
+    [self newGame];
+}
+- (void)buttonClicked:(UIButton*)button
+{
+    if (flagging==YES)[button setTitle:@"X" forState:UIControlStateNormal];
+    else if ([button titleForState:UIControlStateNormal]==@"X"){
+    [button setTitle:@"" forState:UIControlStateNormal];
+    }
+    else{
+        
+    if ([self isMined:button.tag]==YES){
+        //hit
+        [self mineHit];
+    }
+    else {
+        //if not hit
+        [self cleanHit:button];
+    }
+    }    
+}
+-(void)mineHit{
+    for (int d=0;d<100;d++){
+        buttonArray[d].enabled=NO;
+        for (int e=0;e<10;e++){
+            if (buttonArray[d].tag==bombArray[e])[buttonArray[d] setTitle:@"*" forState:UIControlStateDisabled];
+        }
+    }
+    endLabel.text=@"GAME OVER";
+}
+-(void)cleanHit:(UIButton*)button{
+    score++;
+    scoreLabel.text=[NSString stringWithFormat:@"%d",score];
+    if (score==90){
+        //WIN
+        endLabel.text=@"WINNER";
+    }
+    button.enabled=NO;
+    int index=button.tag;
+    if (index==0){
+        //topleft corner
+        int bombCount=0;   
+        if ([self isMined:index+1]==YES)bombCount++;
+        if ([self isMined:index+10]==YES)bombCount++;
+        if ([self isMined:index+11]==YES)bombCount++;
+        if (bombCount==0){
+            [buttonArray[index] setTitle:@"." forState:UIControlStateNormal];
+            if (buttonArray[index+1].isEnabled==YES)[self cleanHit:buttonArray[index+1]];
+            if (buttonArray[index+10].isEnabled==YES)[self cleanHit:buttonArray[index+10]];
+            if (buttonArray[index+11].isEnabled==YES)[self cleanHit:buttonArray[index+11]];
+        }
+        else{
+            [buttonArray[index] setTitle:[NSString stringWithFormat:@"%i",bombCount] forState:UIControlStateNormal];
+            //buttonArray[index].currentTitleColor=[UIColor redColor];
+        } 
+    }
+    else if (index==9){
+        //topright corner
+        int bombCount=0;   
+        if ([self isMined:index-1]==YES)bombCount++;
+        if ([self isMined:index+9]==YES)bombCount++;
+        if ([self isMined:index+10]==YES)bombCount++;
+        if (bombCount==0){
+            [buttonArray[index] setTitle:@"." forState:UIControlStateNormal];
+            if (buttonArray[index-1].isEnabled==YES)[self cleanHit:buttonArray[index-1]];
+            if (buttonArray[index+9].isEnabled==YES)[self cleanHit:buttonArray[index+9]];
+            if (buttonArray[index+10].isEnabled==YES)[self cleanHit:buttonArray[index+10]];
+        }
+        else{
+            [buttonArray[index] setTitle:[NSString stringWithFormat:@"%i",bombCount] forState:UIControlStateNormal];
+        } 
+    }
+    else if (index==90){
+        //bottomleft corner
+        int bombCount=0;   
+        if ([self isMined:index+1]==YES)bombCount++;
+        if ([self isMined:index-9]==YES)bombCount++;
+        if ([self isMined:index-10]==YES)bombCount++;
+        if (bombCount==0){
+            [buttonArray[index] setTitle:@"." forState:UIControlStateNormal];
+            if (buttonArray[index+1].isEnabled==YES)[self cleanHit:buttonArray[index+1]];
+            if (buttonArray[index-9].isEnabled==YES)[self cleanHit:buttonArray[index-9]];
+            if (buttonArray[index-10].isEnabled==YES)[self cleanHit:buttonArray[index-10]];
+        }
+        else{
+            [buttonArray[index] setTitle:[NSString stringWithFormat:@"%i",bombCount] forState:UIControlStateNormal];
+        } 
+    }
+    else if (index==99){
+        //bottomright corner
+        int bombCount=0;   
+        if ([self isMined:index-1]==YES)bombCount++;
+        if ([self isMined:index-10]==YES)bombCount++;
+        if ([self isMined:index-11]==YES)bombCount++;
+        if (bombCount==0){
+            [buttonArray[index] setTitle:@"." forState:UIControlStateNormal];
+            if (buttonArray[index-1].isEnabled==YES)[self cleanHit:buttonArray[index-1]];
+            if (buttonArray[index-10].isEnabled==YES)[self cleanHit:buttonArray[index-10]];
+            if (buttonArray[index-11].isEnabled==YES)[self cleanHit:buttonArray[index-11]];
+        }
+        else{
+            [buttonArray[index] setTitle:[NSString stringWithFormat:@"%i",bombCount] forState:UIControlStateNormal];
+        } 
+    }
+    else if (index<10){
+        //top row
+        int bombCount=0;   
+        if ([self isMined:index+1]==YES)bombCount++;
+        if ([self isMined:index-1]==YES)bombCount++;
+        if ([self isMined:index+9]==YES)bombCount++;
+        if ([self isMined:index+10]==YES)bombCount++;
+        if ([self isMined:index+11]==YES)bombCount++;
+        if (bombCount==0){
+            [buttonArray[index] setTitle:@"." forState:UIControlStateNormal];
+            if (buttonArray[index+1].isEnabled==YES)[self cleanHit:buttonArray[index+1]];
+            if (buttonArray[index-1].isEnabled==YES)[self cleanHit:buttonArray[index-1]];
+            if (buttonArray[index+9].isEnabled==YES)[self cleanHit:buttonArray[index+9]];
+            if (buttonArray[index+10].isEnabled==YES)[self cleanHit:buttonArray[index+10]];
+            if (buttonArray[index+11].isEnabled==YES)[self cleanHit:buttonArray[index+11]];
+        }
+        else{
+            [buttonArray[index] setTitle:[NSString stringWithFormat:@"%i",bombCount] forState:UIControlStateNormal];
+        } 
+    }
+    else if (index>89){
+        //bottom row
+        int bombCount=0;   
+        if ([self isMined:index+1]==YES)bombCount++;
+        if ([self isMined:index-1]==YES)bombCount++;
+        if ([self isMined:index-9]==YES)bombCount++;
+        if ([self isMined:index-10]==YES)bombCount++;
+        if ([self isMined:index-11]==YES)bombCount++;
+        if (bombCount==0){
+            [buttonArray[index] setTitle:@"." forState:UIControlStateNormal];
+            if (buttonArray[index+1].isEnabled==YES)[self cleanHit:buttonArray[index+1]];
+            if (buttonArray[index-1].isEnabled==YES)[self cleanHit:buttonArray[index-1]];
+            if (buttonArray[index-9].isEnabled==YES)[self cleanHit:buttonArray[index-9]];
+            if (buttonArray[index-10].isEnabled==YES)[self cleanHit:buttonArray[index-10]];
+            if (buttonArray[index-11].isEnabled==YES)[self cleanHit:buttonArray[index-11]];
+        }
+        else{
+            [buttonArray[index] setTitle:[NSString stringWithFormat:@"%i",bombCount] forState:UIControlStateNormal];
+        } 
+    }
+    else if ((index % 10)==0){
+        //leftmost column, not a corner
+        int bombCount=0;   
+        if ([self isMined:index+1]==YES)bombCount++;
+        if ([self isMined:index-9]==YES)bombCount++;
+        if ([self isMined:index+10]==YES)bombCount++;
+        if ([self isMined:index-10]==YES)bombCount++;
+        if ([self isMined:index+11]==YES)bombCount++;
+        if (bombCount==0){
+            [buttonArray[index] setTitle:@"." forState:UIControlStateNormal];
+            if (buttonArray[index+1].isEnabled==YES)[self cleanHit:buttonArray[index+1]];
+            if (buttonArray[index-9].isEnabled==YES)[self cleanHit:buttonArray[index-9]];
+            if (buttonArray[index+10].isEnabled==YES)[self cleanHit:buttonArray[index+10]];
+            if (buttonArray[index-10].isEnabled==YES)[self cleanHit:buttonArray[index-10]];
+            if (buttonArray[index+11].isEnabled==YES)[self cleanHit:buttonArray[index+11]];
+        }
+        else{
+            [buttonArray[index] setTitle:[NSString stringWithFormat:@"%i",bombCount] forState:UIControlStateNormal];
+        } 
+    }
+    else if ((index % 10)==9){
+        //rightmost column, not a corner
+        int bombCount=0;   
+        if ([self isMined:index-1]==YES)bombCount++;
+        if ([self isMined:index+9]==YES)bombCount++;
+        if ([self isMined:index+10]==YES)bombCount++;
+        if ([self isMined:index-10]==YES)bombCount++;
+        if ([self isMined:index-11]==YES)bombCount++;
+        if (bombCount==0){
+            [buttonArray[index] setTitle:@"." forState:UIControlStateNormal];
+            if (buttonArray[index-1].isEnabled==YES)[self cleanHit:buttonArray[index-1]];
+            if (buttonArray[index+9].isEnabled==YES)[self cleanHit:buttonArray[index+9]];
+            if (buttonArray[index+10].isEnabled==YES)[self cleanHit:buttonArray[index+10]];
+            if (buttonArray[index-10].isEnabled==YES)[self cleanHit:buttonArray[index-10]];
+            if (buttonArray[index-11].isEnabled==YES)[self cleanHit:buttonArray[index-11]];
+        }
+        else{
+            [buttonArray[index] setTitle:[NSString stringWithFormat:@"%i",bombCount] forState:UIControlStateNormal];
+        } 
+    }
+    else {
+        //not an edge
+        int bombCount=0;   
+        if ([self isMined:index+1]==YES)bombCount++;
+        if ([self isMined:index-1]==YES)bombCount++;
+        if ([self isMined:index+9]==YES)bombCount++;
+        if ([self isMined:index-9]==YES)bombCount++;
+        if ([self isMined:index+10]==YES)bombCount++;
+        if ([self isMined:index-10]==YES)bombCount++;
+        if ([self isMined:index+11]==YES)bombCount++;
+        if ([self isMined:index-11]==YES)bombCount++;
+        if (bombCount==0){
+            [buttonArray[index] setTitle:@"." forState:UIControlStateNormal];
+            if (buttonArray[index+1].isEnabled==YES)[self cleanHit:buttonArray[index+1]];
+            if (buttonArray[index-1].isEnabled==YES)[self cleanHit:buttonArray[index-1]];
+            if (buttonArray[index+9].isEnabled==YES)[self cleanHit:buttonArray[index+9]];
+            if (buttonArray[index-9].isEnabled==YES)[self cleanHit:buttonArray[index-9]];
+            if (buttonArray[index+10].isEnabled==YES)[self cleanHit:buttonArray[index+10]];
+            if (buttonArray[index-10].isEnabled==YES)[self cleanHit:buttonArray[index-10]];
+            if (buttonArray[index+11].isEnabled==YES)[self cleanHit:buttonArray[index+11]];
+            if (buttonArray[index-11].isEnabled==YES)[self cleanHit:buttonArray[index-11]];
+        }
+        else{
+            [buttonArray[index] setTitle:[NSString stringWithFormat:@"%i",bombCount] forState:UIControlStateNormal];
+        } 
+    }
+}
+-(BOOL)isMined:(int)index{
+    BOOL bombed;
+    for (int b=0;b<10;b++){
+        if (buttonArray[index].tag==bombArray[b]){
+            return bombed=YES;
+        }
+    }
+    return bombed=NO;
+}
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    [self newGame];
 }
-
 - (void)viewDidUnload
 {
+    [self setResetGame:nil];
+    [self setScoreLabel:nil];
+    [self setTimeLabel:nil];
+    [self setEndLabel:nil];
+    [self setFlagButton:nil];
+    for (int f=0;f<100;f++){
+        buttonArray[f]=nil;
+    }
+    for (int g=0;g<10;g++){
+        bombArray[g]=0;
+    }
     [super viewDidUnload];
-    // Release any retained subviews of the main view.
-    // e.g. self.myOutlet = nil;
-}
 
-- (void)viewWillAppear:(BOOL)animated
-{
-    [super viewWillAppear:animated];
 }
-
-- (void)viewDidAppear:(BOOL)animated
-{
-    [super viewDidAppear:animated];
-}
-
-- (void)viewWillDisappear:(BOOL)animated
-{
-	[super viewWillDisappear:animated];
-}
-
-- (void)viewDidDisappear:(BOOL)animated
-{
-	[super viewDidDisappear:animated];
-}
-
-- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
-{
-    // Return YES for supported orientations
-    return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
-}
-
 @end
